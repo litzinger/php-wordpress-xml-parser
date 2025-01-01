59c7fdc4e13d5d431a488c2fe3423d193f51a7ff
Brian Litzinger <litzinger@gmail.com>
2025-01-01 17:23:20 -0600

    Changes

--- src/Parser.php
+++ src/Parser.php
@@ -2,9 +2,9 @@
 
 namespace Litzinger\PHPWordpressXMLParser;
 
+use DOMDocument;
 use Litzinger\PHPWordpressXMLParser\Parsers\Regex;
 use Litzinger\PHPWordpressXMLParser\Parsers\SimpleXML;
-use Litzinger\PHPWordpressXMLParser\Parsers\XML;
 
 class Parser
 {
@@ -13,7 +13,24 @@ class Parser
         // Attempt to use proper XML parsers first
         if (extension_loaded('simplexml')) {
             $parser = new SimpleXML;
-            $result = $parser->parseString(simplexml_load_string($content));
+
+            $dom = new DOMDocument();
+            $old_value = null;
+
+            if ( function_exists( 'libxml_disable_entity_loader' ) && PHP_VERSION_ID < 80000 ) {
+                $old_value = libxml_disable_entity_loader( true );
+            }
+
+            $success = $dom->loadXML($content);
+            if ( ! is_null( $old_value ) ) {
+                libxml_disable_entity_loader( $old_value );
+            }
+
+            if ( ! $success || isset($dom->doctype) ) {
+                throw new \Exception( 'There was an error when reading this WXR file' );
+            }
+
+            $result = $parser->parseString(simplexml_import_dom($dom));
 
             return $result;
         }
